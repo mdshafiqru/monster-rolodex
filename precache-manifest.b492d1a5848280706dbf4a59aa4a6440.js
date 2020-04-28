f01adf86bb42d48e75c26a9568bb33fe1fb090ac
Md. Shafiq Khan <mdshafiq.ru@gmail.com>
2020-04-28 16:20:10 +0600

    Updates

---
+++ precache-manifest.b492d1a5848280706dbf4a59aa4a6440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d43586288a61e51966786759e2371d5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ebd6b7615c74f6af884d",
+    "url": "/monster-rolodex/static/css/main.251e904c.chunk.css"
+  },
+  {
+    "revision": "d3b0bf89cc41f1da3c42",
+    "url": "/monster-rolodex/static/js/2.f45e6c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.f45e6c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd6b7615c74f6af884d",
+    "url": "/monster-rolodex/static/js/main.233a9179.chunk.js"
+  },
+  {
+    "revision": "ea391bde138d326494be",
+    "url": "/monster-rolodex/static/js/runtime-main.42b90db1.js"
+  }
+]);
